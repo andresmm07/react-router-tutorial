a17003989f14ce97aee940819b9c6959b75342fb
Andres <andres.mm07@gmail.com>
2016-02-14 17:20:05 -0500

    finished embedded routes and navigation links

--- modules/App.js
+++ modules/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import NavLink from './NavLink'
 
 export default React.createClass({
   render() {
-    return <div>Hey, React Router!</div>
+    return ( 
+      <div>
+        <h1>Gettohub Issues</h1>
+        <ul role="nav">
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/repos">Repos</NavLink></li>
+        </ul>
+        {this.props.children}
+      </div>
+    )
   }
 })
 
